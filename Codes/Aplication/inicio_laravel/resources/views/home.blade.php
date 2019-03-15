30cbf6ddf24835cb8ecf6ffd315ab2543a413903
DigoPC <rodrigo_piranguinho@hotmail.com>
2019-03-15 15:38:02 -0300

    Upload inicial.

---
+++ Codes/Aplication/inicio_laravel/resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('templates/geral_view')
+  
+@section('titulo')
+Home
+@stop
+  
+@section('conteudo')
+<h1>Olá Mundo</h1>
+@stop
---
+++ Codes/Aplication/inicio_laravel/resources/views/templates/geral_view.blade.php
@@ -0,0 +1,48 @@
+<html lang="pt-br">
+    <head> 
+
+        <meta charset="utf-8">
+        <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'>
+
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/normalize.css" rel="stylesheet">
+        <link href="css/font-awesome.css" rel="stylesheet">
+        <link href="css/glyphicons.css" rel="stylesheet">
+        <link href="css/site.css" rel="stylesheet">
+        <link href="css/default.css" rel="stylesheet">
+
+        <title>@yield('titulo')</title>
+
+    </head>
+    <body>
+    <div class='container-fluid div-mestre col-lg-12'>
+        
+        @yield('conteudo')
+
+
+        <div class='copylefts text-center'>
+            <p> Copyleft - 2019 - BY Rodrigo Cândido </p>
+        </div>
+        <div>
+            <script type="text/javascript" src="js/jquery-3.2.1.slim.min.js"></script>
+            <script type="text/javascript" src="js/jquery-3.1.1.min.js"></script>
+            <script type="text/javascript" src="js/jquery.mask.js"></script>
+            <script type="text/javascript" src="js/popper.js"></script>
+            <script type="text/javascript" src="js/popper.min.js"></script>
+            <script type="text/javascript" src="js/bootstrap.bundle.min.js"></script>
+            <script type="text/javascript" src="js/bootstrap.min.js"></script>
+            <script type="text/javascript" src="js/front.js"></script>
+            <script type="text/javascript" src="js/Main.js"></script>
+            <script type="text/javascript" src="js/Init.js"></script>
+            <script type="text/javascript" src="js/Url.js"></script>
+            <script type="text/javascript" src="js/Chart.js"></script>
+            <script type="text/javascript" src="js/legend.js"></script>
+            <script type="text/javascript" src="js/bootstrap-datepicker.min.js"></script>
+            <script type="text/javascript" src="js/bootstrap-datepicker.pt-BR.min.js"></script>
+            <script type="text/javascript" src="js/chosen.jquery.js"></script>
+            <script type="text/javascript" src="js/chosen.proto.js"></script>
+
+        </div>
+    </div>
+    </body> 
+</html>
